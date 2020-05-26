2c57780ff216ef2ba4d99e6b90a9926ad68411d9
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-05-26 13:57:30 +0200

    made setup method in RadarTest
    Signed-off-by: Kowalczyk.Bartosz <21577@edu.p.lodz.pl>

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,30 +1,35 @@
 package edu.iis.mto.multithread;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
 import static org.mockito.Mockito.*;
 
 public class RadarTest {
+    PatriotBattery batteryMock;
+    Scud enemyMissile;
+
+    @BeforeEach
+    void setUp() {
+        batteryMock = mock(PatriotBattery.class);
+        enemyMissile = new Scud();
+    }
 
     @RepeatedTest(20)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock, timeout(100).times(10)).launchPatriot(enemyMissle);
+        radar.notice(enemyMissile);
+        verify(batteryMock, timeout(100).times(10)).launchPatriot(enemyMissile);
     }
 
 
     @RepeatedTest(15)
     public void launchPatriotOnceWhenNoticesAScudMissleXTimesUsingBetterRadar() {
         int howManyMissiles = 100;
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle, howManyMissiles);
-        verify(batteryMock, timeout(10*howManyMissiles).times(howManyMissiles)).launchPatriot(enemyMissle);
-
+        radar.notice(enemyMissile, howManyMissiles);
+        verify(batteryMock, timeout(10 * howManyMissiles).times(howManyMissiles))
+                .launchPatriot(enemyMissile);
     }
 
 }
