d43eeff0593529b41845095b08ce3e60354c6733
liberadzkih <liberadzkih@gmail.com>
2020-09-14 17:30:42 +0200

    Refactor RadarTest and added RepeatedTests

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,27 +1,34 @@
 package edu.iis.mto.multithread;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 public class RadarTest {
 
-    @Test
-    public void launchPatriotAtleastOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock, timeout(100).atLeast(1)).launchPatriot(enemyMissle);
+    PatriotBattery batteryMock;
+    Scud enemyMissile;
+
+    @BeforeEach
+    public void setUp() {
+        batteryMock = mock(PatriotBattery.class);
+        enemyMissile = new Scud();
     }
 
-    @Test
+    @RepeatedTest(30)
+    public void launchPatriotAtleastOnceWhenNoticesAScudMissle() {
+        Radar radar = new Radar(batteryMock);
+        radar.notice(enemyMissile);
+        verify(batteryMock, timeout(100).atLeast(1)).launchPatriot(enemyMissile);
+    }
+
+    @RepeatedTest(30)
     public void launchPatriotTenTimesWhenNoticesAScudMissle_withBetterRadar() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar betterRadar = new BetterRadar(batteryMock);
-        Scud enemyMissle = new Scud();
-        betterRadar.notice(enemyMissle);
-        verify(batteryMock, timeout(100).times(10)).launchPatriot(enemyMissle);
+        betterRadar.notice(enemyMissile);
+        verify(batteryMock, timeout(100).times(10)).launchPatriot(enemyMissile);
     }
 
 }
